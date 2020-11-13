1297f735db61a3bc82ad8b9e5fc4edaf8fd94c3b
supercoder3000 <mail.jhahn@googlemail.com>
2020-11-14 00:00:18 +0100

    first version

--- _js/vendors/particles.js
+++ _js/vendors/particles.js
@@ -34,7 +34,7 @@ var pJS = function(tag_id, params) {
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#ff0000'
+          color: '#00ff00'
         },
         polygon: {
           nb_sides: 5
